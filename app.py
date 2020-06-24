c1c505b1436d3048a0e298ac3db7463e25a80003
Nicolás Antonelli <niconelli2@gmail.com>
2020-06-24 03:55:47 -0300

    Layout & Templates

--- app.py
+++ app.py
@@ -2,11 +2,20 @@ from flask import Flask
 from flask import render_template
 from datetime import datetime
 import re
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template("home.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
 
 @app.route("/hello")
 @app.route("/hello/<name>")
