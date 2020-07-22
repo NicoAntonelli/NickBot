6981031cf8dc158a6e6b2ed03023b80d428ad194
Nicolás Antonelli <niconelli2@gmail.com>
2020-07-21 23:12:27 -0300

    Added WhatsApp/SMS Bot

--- simple_app/views.py
+++ simple_app/views.py
@@ -1,6 +1,8 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from twilio.twiml.messaging_response import MessagingResponse
 from . import app
+import requests
 
 
 @app.route("/")
@@ -27,3 +29,32 @@ def hello_there(name=None):
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
+
+
+# SMS / WhatsApp Bot
+@app.route("/api/bot", methods=["POST"])
+def bot():
+    incoming_msg = request.values.get("Body", "").lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    if "quote" in incoming_msg:
+        # Return a quote
+        r = requests.get("https://api.quotable.io/random")
+        if r.status_code == 200:
+            data = r.json()
+            quote = f'{data["content"]} ({data["author"]})'
+        else:
+            quote = "I could not retrieve a quote at this time, sorry."
+        msg.body(quote)
+        responded = True
+    if "cat" in incoming_msg:
+        # Return a cat pic
+        msg.media("https://cataas.com/cat")
+        responded = True
+    if not responded:
+        if "hello" in incoming_msg:
+            msg.body("Hello World, curious human! Ask me about cats or famous quotes!")
+        else:
+            msg.body("I only know about famous quotes and cats, sorry!")
+    return str(resp)
